013eb902dd45658dc2e9d70806e4c7bbd8ce5023
APPROX Coding <chzain0319@gmail.com>
2025-06-20 14:24:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,155 @@
+Shery.imageEffect("#back", {
+  style: 5,
+  config: {
+    a: { value: 2, range: [0, 30] },
+    b: { value: -0.95, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 2.1241611399783085 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    growSize: { value: 4.01, range: [1, 15] },
+    durationOut: { value: 1.03, range: [0.1, 5] },
+    durationIn: { value: 1.03, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.3, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.74, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2], _gsap: { id: 3 } },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 14.05, range: [0, 100] },
+  },
+  gooey: true,
+});
+
+// locomotive js - smooth scrolling
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#scroll"),
+  smooth: true,
+});
+
+// gsap to animate
+gsap.from(".nlink", {
+  stagger: .3,
+  y: 30,
+  duration:1,
+  opacity: 0,
+});
+
+Shery.imageEffect("#imgntext img", {
+  style: 3,
+  config: {
+    uFrequencyX: { value: 12, range: [0, 100] },
+    uFrequencyY: { value: 12, range: [0, 100] },
+    uFrequencyZ: { value: 10, range: [0, 100] },
+    geoVertex: { range: [1, 64], value: 32 },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.75 },
+    gooey: { value: false },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.6, range: [1, 5] },
+    scrollType: { value: 0 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 0 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0.002, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+});
+
+Shery.imageEffect(".imgeff img", {
+  style: 5,
+  config: {
+    a: { value: 2, range: [0, 30] },
+    b: { value: -0.69, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.6666666666666666 },
+    gooey: { value: false },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.33, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0.002, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+});
+
+gsap.from("#imgntext img", {
+  y: "70",
+  opacity: 0,
+  duration: 2,
+  ease: Expo,
+});
+
+Shery.imageEffect("#bimg", {
+  style: 5,
+  config: {
+    a: { value: 0.74, range: [0, 30] },
+    b: { value: -0.97, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 2.326662723463724 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    growSize: { value: 2.74, range: [1, 15] },
+    durationOut: { value: 0.55, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.2, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.74, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2], _gsap: { id: 33 } },
+    discard_threshold: { value: 0.42, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.43, range: [0, 2] },
+    noise_scale: { value: 10.74, range: [0, 100] },
+  },
+  gooey: true,
+});
+
+document
+  .querySelector("#ftext button")
+  .addEventListener("mouseover", function () {
+    gsap.to("#future video", {
+      opacity: 1,
+      duration: 1,
+    });
+  });
+
+document
+  .querySelector("#ftext button")
+  .addEventListener("mouseleave", function () {
+    gsap.to("#future video", {
+      opacity: 0,
+      duration: 1,
+    });
+  });
+
+// shery js to animate accordingly
